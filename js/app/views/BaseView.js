30461835e1a6e94985cfd192afcc48e23726163f
Nikos <rpEK_007@mail.ru>
2012-03-04 00:11:09 +0200

    I hope the final variant :)

--- js/app/views/BaseView.js
+++ js/app/views/BaseView.js
@@ -87,16 +87,21 @@ define(['models/TreeNodeModel', 'collections/TreeCollection', 'models/TreeNodeMo
 			this.renderer = new THREE.CanvasRenderer();
 			this.renderer.setSize(window.innerWidth, window.innerHeight);
 			this.container.appendChild(this.renderer.domElement);
-            var t = setTimeout("$('#navigator').animate({left:'-=105px'});",2000);
+            var t = setTimeout("$('#navigator').animate({left:'-=105px'},function(){$('#navigator').css('background-color', '#1A3457');});",2000);
 		},
         
         navShow: function(){
-            if (!this.showedNav)$('#navigator').animate({left:'+=105px'});
+            if (!this.showedNav){
+                $('#navigator').css("background-color", "#617c83");
+                $('#navigator').animate({left:'+=105px'});
+            }
             this.showedNav = true;
         },
         
         navHide: function(){
-            if (this.showedNav)$('#navigator').animate({left:'-=105px'});
+            if (this.showedNav) {
+                $('#navigator').animate({left:'-=105px'},function(){$('#navigator').css("background-color", "#1A3457");});
+            }
             this.showedNav = false;
         },
         
