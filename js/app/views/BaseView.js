b3ebe07532923aa74f023b2da9ff5dc4b75eecda
Nikos <rpEK_007@mail.ru>
2012-03-03 23:31:20 +0200

    Slide 2 sec after loading

--- js/app/views/BaseView.js
+++ js/app/views/BaseView.js
@@ -87,15 +87,16 @@ define(['models/TreeNodeModel', 'collections/TreeCollection', 'models/TreeNodeMo
 			this.renderer = new THREE.CanvasRenderer();
 			this.renderer.setSize(window.innerWidth, window.innerHeight);
 			this.container.appendChild(this.renderer.domElement);
+            var t = setTimeout("$('#navigator').animate({left:'-=110px'});",2000);
 		},
         
         navShow: function(){
-            if (!this.showedNav)$('#navigator').animate({left:'+=120px'});
+            if (!this.showedNav)$('#navigator').animate({left:'+=110px'});
             this.showedNav = true;
         },
         
         navHide: function(){
-            if (this.showedNav)$('#navigator').animate({left:'-=120px'});
+            if (this.showedNav)$('#navigator').animate({left:'-=110px'});
             this.showedNav = false;
         },
         
